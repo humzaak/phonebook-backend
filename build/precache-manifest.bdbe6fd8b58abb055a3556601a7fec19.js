8e609b6ee0518a4253f02028291c18391e5b640c
Humza Akhtar <humzaakhtar1@gmail.com>
2020-05-03 22:44:23 +0800

    upload full stack including build

---
+++ build/precache-manifest.bdbe6fd8b58abb055a3556601a7fec19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389d7d64ae580e88a7625e27ec12a0bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "650b8ba955905a1332bf",
+    "url": "/static/css/main.40f60b5e.chunk.css"
+  },
+  {
+    "revision": "fa5d6439d85375a76b70",
+    "url": "/static/js/2.cbd3332e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cbd3332e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650b8ba955905a1332bf",
+    "url": "/static/js/main.70e9e8a0.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
